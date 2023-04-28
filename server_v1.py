c15b5663c60617958985a665c53b5bb9d2b3e78c
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-04-28 13:20:25 +0800

    use the post to avoid the json data cashed by by browser

--- server_v1.py
+++ server_v1.py
@@ -11,7 +11,53 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_timeline():
-    return render_template('index_v1.html')
+    eventJson = {
+        "title": {
+                "text": {
+                    "headline": "Example timeline",
+                    "text":     "Events 123 in the 7th century \
+                        from https://en.wikipedia.org/wiki/Timeline_of_the_Middle_Ages"
+                }
+        },
+        "events": [
+            {
+                "start_date": {
+                    "year":			"2015",
+                    "month":		"01"
+                },
+                "media": {
+                    "url": "static/img/stick-man.png"
+                },
+                "text": {
+                    "headline": "Example with own image",
+                    "text": "Long conflict leaves both empires exhausted and unable to cope with the newly united Arab armies under Islam in the 630s"
+                }
+            },
+            {
+                "start_date": {
+                    "year":			"2015",
+                    "month":		"02"
+                },
+                "end_date": {
+                    "year":			"2015",
+                    "month":		"03"
+                },
+                "media": {
+                    "url": "https://en.wikipedia.org/wiki/Grand_Canal_(China)"
+                },
+                "background": {
+                    "opacity":"50",
+                    "url": "https://upload.wikimedia.org/wikipedia/commons/a/ad/Sui_Wendi_Tang.jpg"
+                },
+                "text": {
+                    "headline": "Grand Canal in China is fully completed",
+                    "text": "Its main role throughout its history was the transport of grain to the capital."
+                }
+            }
+    
+        ]
+    }
+    return render_template('index_v1.html', posts=eventJson)
 
 if __name__ == "__main__":	
 	app.run(port = 5000, host = '0.0.0.0', use_reloader=True, threaded=True, debug=True)
